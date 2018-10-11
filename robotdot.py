cc74cb0bef8cda71e0753e436cb7f9390e5a8375
LukasPol <lukaspaespol@gmail.com>
2018-10-11 20:30:58 -0300

    add perfil and hastag

--- robotdot.py
+++ robotdot.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from selenium.webdriver.common.keys import Keys
-import time
 import random
 from time import sleep
+from comments import comments
 
 
 class Robotdot:
@@ -11,7 +11,8 @@ class Robotdot:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.driver = webdriver.Chrome('/home/shakan/Downloads/chromedriver')
+        #self.driver = webdriver.Chrome('/home/shakan/Downloads/chromedriver')
+        self.driver = webdriver.Chrome('chromedriver.exe')
 
     def login(self):
         driver = self.driver
@@ -23,14 +24,14 @@ class Robotdot:
         sleep(2)
         enter_submit = driver.find_element_by_class_name('Igw0E')
         enter_submit.submit()
-        # sleep(2)
-        # notification = driver.find_element_by_class_name('aOOlW')
-        # notification.click()
         sleep(2)
 
-    def curtir(self, hashtag):
+    def curtir(self, hashtag, esc):
         driver = self.driver
-        driver.get('https://www.instagram.com/explore/tags/' + hashtag + '/')
+        if esc == 2:
+            driver.get('https://www.instagram.com/explore/tags/' + hashtag + '/') # para hashtag
+        if esc == 1:
+            driver.get('https://www.instagram.com/'+hashtag+'/') # para perfis
         sleep(2)
 
         #um loop para baixar a página e atualizar as fotos - loop to looking for photos and update them
@@ -45,9 +46,7 @@ class Robotdot:
         photos_links = [href for href in photos_links if hashtag in href]
         print(hashtag + ' photos ' + str(len(photos_links)))
 
-        comments = ['Show!', 'Caramba!', 'Legal', 'Curti demais', 'Sensacional', 'Demais mesmo =D', 'Amei S2',
-                    'Manowwwwww!!!', 'Loucura ;D', 'Sinistrooooo', 'É bom demais', 'Não tem coisa melhor', 'Muito bom kkkkk',
-                    '=D','the ' + hashtag + ' is the best!']
+                    
         for photo_link in photos_links:
             driver.get(photo_link)
             driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
@@ -58,6 +57,8 @@ class Robotdot:
             except NoSuchElementException:
                 pass
 
+
+            # == Comments
             try:
                 sleep(random.randint(3, 7))
                 comment_write = lambda: driver.find_element_by_xpath('//textarea[@aria-label="Adicione um comentário..."]')
@@ -66,26 +67,31 @@ class Robotdot:
                 comment_write().send_keys(random.choice(comments))
                 comment_write().send_keys(Keys.ENTER)
 
-
-
             except StaleElementReferenceException and NoSuchElementException as e:
                 print(e)
                 sleep(2)
 
 
-
+            # == Likes
             try:
                 sleep(random.randint(2, 4))
                 like_button = lambda: driver.find_element_by_xpath('/html/body/span/section/main/div/div/article/div[2]/section[1]/span[1]/button').click()
                 like_button().click()
-
 
             except Exception as e:
                 sleep(2)
 
 
 
-ig = Robotdot() #put here your username and password as arguments - coloque seu usuário e senha
-ig.login()
-ig.curtir() #here, you put the hashtag that you are looking for - Coloque aqui a hashtag que vc está procurando
+user = input('Username: ')
+password = input('Password: ')
 
+while True:
+    esc = int(input('[1]Perfil ou [2]tag: '))
+    if esc ==1 or esc==2:
+        break
+
+tag = input('Hastag ou perfil: ')
+ig = Robotdot(user, password) #put here your username and password as arguments - coloque seu usuário e senha
+ig.login()
+ig.curtir(tag,esc) # here, you put the hashtag that you are looking for - Coloque aqui a hashtag que vc está procurando
