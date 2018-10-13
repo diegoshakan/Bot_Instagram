29da018f76fb4db1f4510d4af364b6f3d18dcbfb
Diego Silva de Araujo <diego_shakan@hotmail.com>
2018-10-13 00:05:48 -0300

    Está funcionando, modificando para classes e resolvendo os bugs

--- robotdot.py
+++ robotdot.py
@@ -1,73 +1,63 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import random
 from time import sleep
 
 
-#You must download the chromedriver if you will use the navigator Chrome.
-#driver = webdriver.Chrome('/home/shakan/Downloads/chromedriver') #This parameter is necessary, because I use the navigator Chrome.
-driver = webdriver.Chrome('chromedriver.exe')
+class Robotdot:
 
-# this is url of Instagram
-driver.get(
-    'https://www.instagram.com/accounts/login/?hl=pt-br&source=auth_switcher')
-#x= input()
-'''Here you must insert your name user and your password'''
-name = input('Enter with your username: ')
-password = input('Enter with your password: ')
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Chrome('/home/shakan/Downloads/chromedriver')
 
-'''the input of name of user and password is same class of name. For this, I need make a 'for' trough function 'find_elements'.'''
-acess = driver.find_elements_by_class_name('_2hvTZ')
+    def login(self):
+        driver = self.driver
+        driver.get('https://www.instagram.com/accounts/login/?hl=pt-br&source=auth_switcher')
+        username_input = driver.find_element_by_xpath('//input[@name="username"]')
+        username_input.send_keys(self.username)
+        password_input = driver.find_element_by_xpath('//input[@name="password"]')
+        password_input.send_keys(self.password)
+        enter_submit = driver.find_element_by_class_name('Igw0E')
+        enter_submit.submit()
+        sleep(2)
+        notification = driver.find_element_by_class_name('aOOlW')
+        notification.click()
+        sleep(2)
 
-# I found two class, so the first position in my array is the input user name.
-user_name = acess[0]
-user_name.send_keys(name)
+    def curtir(self, hashtag):
+        driver = self.driver
+        driver.get('https://www.instagram.com/explore/tags/' + hashtag + '/')
+        sleep(2)
 
-# Logicaly, the second position is the input for password
-password_name = acess[1]
-password_name.send_keys(password)
+        #um loop para baixar a página e atualizar as fotos - loop to looking for photos and update them
+        for i in range(1, 2):
+            driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            sleep(2)
 
-login_acess = driver.find_element_by_class_name('Igw0E')
-login_acess.submit()
 
-# sleep here, because the bot need a time for the cause velocity of internet.
-# sleep(2.0)
-# notification = driver.find_element_by_class_name('aOOlW')
-# notification.click()
+        #após atualizar as fotos na página, receber seus respectivos endereços
+        links = driver.find_elements_by_tag_name('a')
+        photos_links = [elem.get_attribute('href') for elem in links]
+        photos_links = [href for href in photos_links if hashtag in href]
+        print(hashtag + ' photos ' + str(len(photos_links)))
 
-# Test of like
-sleep(2.0)
 
-"""
-like = driver.find_elements_by_class_name('coreSpriteHeartOpen')
-print(len(like))
-like[0].click()
-like = driver.find_elements_by_class_name('coreSpriteHeartOpen')
-print(len(like))
-sleep(2.0)
-like[1].click()
+        for photo_link in photos_links:
+            driver.get(photo_link)
+            driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            try:
+                time.sleep(random.randint(2, 4))
+                like_button = lambda: driver.find_element_by_xpath('/html/body/span/section/main/div/div/article/div[2]/section[1]/span[1]/button').click()
+                like_button().click()
 
-"""
+            except Exception as e:
+                sleep(2)
 
-# pega a quantidade de fotos q quer curtir
-quant = int(input('Quantas fotos você quer curtir? '))
 
-# Um contador começando do zero
-# que vai até a quant. de fotos a serem curtidas
-# e pelo x que será identificado as fotos na lista "like"
-x = 0
 
-while x < quant:
-    # pegar as fotos
-    like = driver.find_elements_by_class_name('coreSpriteHeartOpen')
-    t = len(like)
-    # for para percorrer a lista
-    for i in range(t):
-        # caso base para parar o programa
-        if x == quant:
-            break
-        sleep(2.0)
+ig = Robotdot() #put here your username and password as arguments - coloque seu usuário e senha
+ig.login()
+ig.curtir() #here, you put the hashtag that you are looking for - Coloque aqui a hashtag que vc está procurando
 
-        # curtindo as fotos
-        like[x].click()
-
-        # aumentando contador
-        x += 1
