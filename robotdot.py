385190a0b3448b02e53fac1dbccb609a2b0b9a95
LukasPol <lukaspaespol@gmail.com>
2018-10-13 15:40:09 -0300

    Separando as funções [comment/like/page]

--- robotdot.py
+++ robotdot.py
@@ -1,7 +1,6 @@
 from imports import *
 
 class Robotdot:
-
     def __init__(self, username, password):
         self.username = username
         self.password = password
@@ -19,8 +18,48 @@ class Robotdot:
         enter_submit = driver.find_element_by_class_name('Igw0E')
         enter_submit.submit()
         sleep(2)
+    
+    def comment(self):
+        driver = self.driver
+        """try:
+            sleep(random.randint(3, 7))
+            def comment_button(): return driver.find_element_by_link_text(
+                'Comentar')
+            comment_button().click()
+        except NoSuchElementException:
+            pass"""
+            
+        # == Comments
+        try:
+            sleep(random.randint(3, 7))
+            def comment_write(): return driver.find_element_by_xpath('//textarea[@aria-label="Adicione um comentário..."]')
+            comment_write().clear()
+            sleep(random.randint(3, 10))
+            text = random.choice(comments)
+            for i in text:
+                comment_write().send_keys(i)
+                sleep(0.3)
+            comment_write().send_keys(Keys.ENTER)
 
-    def curtir(self, hashtag, esc):
+        except StaleElementReferenceException and NoSuchElementException as e:
+            print(e)
+            sleep(2)
+
+
+    def like(self):
+        driver = self.driver
+        # == Likes
+        try:
+            sleep(random.randint(2, 4))
+
+            def like_button(): return driver.find_element_by_xpath(
+                '/html/body/span/section/main/div/div/article/div[2]/section[1]/span[1]/button').click()
+            like_button().click()
+
+        except Exception as e:
+            sleep(2)
+
+    def page(self, hashtag, esc):
         driver = self.driver
         if esc == 2:
             driver.get('https://www.instagram.com/explore/tags/' + hashtag + '/') # para hashtag
@@ -44,48 +83,6 @@ class Robotdot:
         for photo_link in photos_links:
             driver.get(photo_link)
             driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-            try:
-                sleep(random.randint(3, 7))
-                comment_button = lambda: driver.find_element_by_link_text('Comentar')
-                comment_button().click()
-            except NoSuchElementException:
-                pass
-
-
-            # == Comments
-            try:
-                sleep(random.randint(3, 7))
-                comment_write = lambda: driver.find_element_by_xpath('//textarea[@aria-label="Adicione um comentário..."]')
-                comment_write().clear()
-                sleep(random.randint(3, 10))
-                comment_write().send_keys(random.choice(comments))
-                comment_write().send_keys(Keys.ENTER)
-
-            except StaleElementReferenceException and NoSuchElementException as e:
-                print(e)
-                sleep(2)
-
-
-            # == Likes
-            try:
-                sleep(random.randint(2, 4))
-                like_button = lambda: driver.find_element_by_xpath('/html/body/span/section/main/div/div/article/div[2]/section[1]/span[1]/button').click()
-                like_button().click()
-
-            except Exception as e:
-                sleep(2)
-
-
-
-user = input('Username: ')
-password = input('Password: ')
-
-while True:
-    esc = int(input('[1]Perfil ou [2]tag: '))
-    if esc ==1 or esc==2:
-        break
-
-tag = input('Hastag ou perfil: ')
-ig = Robotdot(user, password) #put here your username and password as arguments - coloque seu usuário e senha
-ig.login()
-ig.curtir(tag,esc) # here, you put the hashtag that you are looking for - Coloque aqui a hashtag que vc está procurando
+            
+            self.comment()
+            self.like()
